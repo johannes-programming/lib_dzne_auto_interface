2f85c8a894d7e4f008d1933af900019e1496f8b7
Johannes Horler <johannes.horler@dzne.de>
2023-06-11 23:57:50 +0200

    First attempt to reintroduce tkinter

--- src/lib_dzne_auto_interface/__init__.py
+++ src/lib_dzne_auto_interface/__init__.py
@@ -1,5 +1,8 @@
 import argparse as _ap
 import inspect as _ins
+import tkinter as _tk
+import tkinter.messagebox as _msgbox
+import tkinter.ttk as _ttk
 
 
 class Information(object):
@@ -93,6 +96,8 @@ class _Knot(object):
         )
     def parse(self, args, *, add_help=False):
         return vars(self.parser(add_help=add_help).parse_args(args))
+    def frame(self, master):
+        return type(self)._Frame(master=master, knot=self)
     @staticmethod
     def _details_from_annotation(annotation):
         if annotation is _ins.Parameter.empty:
@@ -106,11 +111,149 @@ class _Knot(object):
     def subknots(self):
         return list(self._subknots)
 
-class _Argument(_Knot):
+
+class Checkbutton(_ttk.Checkbutton):
     def __init__(self, *args, **kwargs):
+        self.intVar = _tk.IntVar()
+        super().__init__(*args, variable=self.intVar, onvalue=1, offvalue=0, **kwargs)
+        self.active = False
+    @property
+    def checked(self):
+        return bool(self.intVar.get())
+class HelpButton(_ttk.Button):
+    @classmethod
+    def make(cls, *args, message, **kwargs):
+        if message is None:
+            return None
+        return cls(*args, message=message, **kwargs)
+    def __init__(self, *args, title, message, **kwargs):
+        super().__init__(*args, command=self.show_help, **kwargs)
+        self.title = title
+        self.message = message
+    def show_help(self):
+        _msgbox.showinfo(
+            title=self.title,
+            message=self.message,
+        )
+class SingleInput(_tk.Entry):
+    def __init__(self, *args, **kwargs):
+        self.stringVar = _tk.StringVar()
+        super().__init__(*args, textvariable=self.stringVar, **kwargs)
+    def get_args(self):
+        return [self.stringVar.get()]
+    @property
+    def active(self):
+        return self._active
+    @active.setter
+    def active(self, value):
+        value = bool(value)
+        if value:
+            self.config(state='normal')
+        else:
+            self.config(state="disabled")
+        self._active = value
+class MultiInput(_tk.Text):
+    @property
+    def active(self):
+        return self._active
+    @active.setter
+    def active(self, value):
+        value = bool(value)
+        if value:
+            self.config(state='normal')
+        else:
+            self.config(state="disabled")
+        self._active = value
+    def get_args(self):
+        ans = self.get("1.0", "end-1c")
+        ans = ans.split('\n')
+        return ans
+
+
+
+class _Argument(_Knot):
+    class _Frame(LabelFrame):
+        def __init__(self, master, knot):
+            super().__init__(master, text=knot.dest)
+            self.knot = knot
+            self.helpButton = self._add_helpButton()
+            self.checkbutton = self._add_checkbutton()
+            self.inputWidget = self._add_inputWigdet()
+        @property
+        def option_string(self):
+            values = self.knot.option_strings
+            if len(values):
+                return values[0]
+            else:
+                return None
+        @property
+        def active:
+            if self.checkbutton is None:
+                return None
+            return self.checkbutton.active
+        def get_args(self):
+            ans = list()
+            if self.active is False:
+                return ans
+            if self.option_string is not None:
+                ans.append(self.option_string)
+            if self.inputWidget is not None:
+                ans += self.inputWidget.get_args()
+            return ans
+        def parse(self):
+            args = self.get_args()
+            kwargs = self.knot.parse(args)
+            return kwargs
+        def check_change(self):
+            self.inputWidget.active = self.checkbutton.checked
+        def _add_helpButton(self):
+            if self.knot.help is None:
+                return None
+            ans = HelpButton(
+                self,
+                title=f"help :{self.knot.dest}",
+                message=self.knot.help,
+            )
+            ans.pack(
+                side='right',
+                padx=10,
+                pady=10,
+            )
+            return ans
+        def _add_checkbutton(self):
+            if self.knot.required:
+                return None
+            ans = Checkbutton(
+                self, 
+                command=self.check_change,
+            )
+            ans.pack(
+                side='right',
+                padx=10,
+                pady=10,
+            )
+            return ans
+        def _add_inputWigdet(self):
+            if self.knot.action == 'store_true':
+                return None
+            if self.knot.action == 'store_false':
+                return None
+            if self.knot.nargs is None:
+                Ans = SingleInput
+            else:
+                Ans = MultiInput
+            ans = Ans(self)
+            ans.pack(
+                padx=10,
+                pady=10,
+            )
+    def __init__(self, *args, **kwargs):
+        self._subknots = list()
         info = Information()
         info.kwargs = dict(*args, **kwargs)
         info.args = info.pop('option_strings', [])
+
+        # action
         info['action'] = info.get('action', 'store')
         self._action_string = info['action']
         if self._action_string not in (
@@ -122,9 +265,9 @@ class _Argument(_Knot):
             #'append_const',
         ):
             raise ValueError()
+
         self._parser = self._make_parser()
         self._action = info.exec(self._parser.add_argument)
-        self._subknots = list()
     @property
     def positional(self):
         return not bool(len(self.option_strings))
@@ -143,6 +286,9 @@ class _Argument(_Knot):
     @property
     def help(self):
         return self._action.help
+    @property
+    def required(self):
+        return self._action.required
     @classmethod
     def of_return(cls, annotation, *, details={}):
         if annotation is _ins.Parameter.empty:
@@ -151,6 +297,28 @@ class _Argument(_Knot):
         return _Argument(**ann, **details)
 
 class _Parameter(_Knot):
+    class _Frame(_tk.Frame):
+        def __init__(self, master, *, knot):
+            super().__init__(master)
+            self.knot = knot
+            self.subframes = list()
+            for item in enumerate(self.knot.subknots):
+                self.subframes.append(self._subframe(*item, len(self.knot.subknots)))
+        def _subframe(self, index, argument, length):
+            ans = argument.frame(self)
+            padN = 0 if (index == 0) else 10
+            padS = 0 if (index == length - 1) else 10
+            ans.pack(
+                side='top',
+                padx=(0, 0),
+                pady=(padN, padS),
+            )
+        def parse(self):
+            ans = dict()
+            for subframe in self.subframes:
+                kwargs = subframe.parse()
+                ans = dict(**ans, **kwargs)
+            return ans
     def __init__(self, value):
         self._subknots = self._get_subknots(value)
         parents = [x.parser(add_help=False) for x in self._subknots]
@@ -218,9 +386,10 @@ class _Main(_Knot):
         kwargs = self.parse(args, add_help=True)
         self._run(kwargs)
     def run_gui(self):
-        #implement!
-        #launch root window
-        raise NotImplementedError()
+        root = _tk.Tk()
+        frame = self.frame(root)
+        frame.pack(fill=_tk.BOTH, expand=True)
+        root.mainloop()
     def _run(self, kwargs):
         raise NotImplementedError()
     @property
@@ -229,6 +398,55 @@ class _Main(_Knot):
 
 
 class _Callable(_Main):
+    class _Frame(_tk.Frame):
+        def __init__(self, master, *, knot):
+            super().__init__(master)
+            self.knot = knot
+            self._init()
+        def _init(self):
+            self.buttonFrame = _tk.Frame(self)
+            self.buttonFrame.pack(
+                side='bottom',
+                fill=_tk.X,
+            )
+            self.helpButton = HelpButton(
+                self.buttonFrame,
+                title="help",
+                message=self.description,
+            )
+            self.helpButton.pack(
+                side='right',
+                padx=10,
+                pady=10,
+            )
+            self.goButton = _ttk.Button(
+                self.buttonFrame,
+                command=self.go,
+            )
+            self.goButton.pack(
+                side='right',
+                padx=10,
+                pady=10,
+            )
+            self.subframes = list()
+            for subknot in self.knot.subknots:
+                subframe = subknot.frame(master=self, knot=self.knot)
+                subframe.pack(
+                    side='top',
+                    fill=_tk.X,
+                    padx=10,
+                    pady=10,
+                )
+                self.subframes.append(subframe)
+        def parse(self):
+            ans = dict()
+            for subframe in self.subframes:
+                kwargs = subframe.parse()
+                ans = dict(**ans, **kwargs)
+            return ans
+        def go(self):
+            kwargs = self.parse()
+            self.knot._run(kwargs)
     def __init__(self, value, return_details):
         self._value = value
         signature = _ins.signature(value)
@@ -239,12 +457,9 @@ class _Callable(_Main):
         )
         parents = [x.parser(add_help=False) for x in self.subknots]
         self._parser = self._make_parser(
-            description=value.__doc__,
+            description=self.description,
             parents=parents,
         )
-    def _read_gui(self):
-        raise NotImplementedError()
-        #implement!
     def _run(self, kwargs):
         if self.argument_of_return is None:
             outfile = None
@@ -261,9 +476,6 @@ class _Callable(_Main):
             return
         result = outfile.fileDataType(result)
         outfile.save(result)
-    def _go(self):
-        kwargs = self._read_gui()
-        self._run(kwargs)
     @property
     def _subknots(self):
         ans = self.parameters
@@ -276,10 +488,57 @@ class _Callable(_Main):
     @property
     def argument_of_return(self):
         return self._argument_of_return
+    @property
+    def description(self):
+        return self._value.__doc__
 
 class _Uncallable(_Main):
+    class _Frame(_tk.Frame):
+        def __init__(self, master, *, knot, **kwargs):
+            super().__init__(master, **kwargs)
+            self.knot = knot
+            self._init()
+        def _init(self):
+            self.buttonFrame = self._add_buttomFrame()
+            self.helpButton = self._add_helpButton()
+            self.notebook = self._add_notebook()#ttk.Notebook(container,**options)
+            for name, subknot in self.mains.items():
+                subframe = subknot.frame(self)
+                self.notebook.add(subframe, text=name)
+        def _add_notebook(self):
+            ans = _ttk.Notebook(self)
+            ans.pack(
+                side='top',
+                file='both',
+                padx=10,
+                pady=10,
+            )
+            return ans
+        def _add_buttomFrame(self):
+            ans = _tk.Frame(self)
+            ans.pack(
+                side='bottom',
+                fill='x',
+                padx=0,
+                pady=0,
+            )
+            return ans
+        def _add_helpButton(self):
+            if self.description is None:
+                return None
+            ans = HelpButton(
+                self.buttonFrame,
+                title="help",
+                message=self.description,
+            )
+            ans.pack(
+                side='right',
+                padx=10,
+                pady=10,
+            )
     def __init__(self, value, return_details):
         self._dest = value._dest
+        self._description = value.__doc__
         self._mains = dict()
         parser = self._make_parser()
         subparsers = parser.add_subparsers(dest=value._dest)
@@ -302,6 +561,9 @@ class _Uncallable(_Main):
     def _run(self, kwargs):
         value = kwargs.pop(self.dest)
         return self._mains[value]._run(kwargs)
+    @property
+    def description(self):
+        return self._description
     @property
     def dest(self):
         return self._dest
